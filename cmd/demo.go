ac637d980196ece52b8f89fc1b00477a26f0db86
Phanarat Pak <phanpak@gmail.com>
2020-03-13 12:28:02 +0700

    Add demo main

---
+++ cmd/demo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/phanpak/secret"
+)
+
+func main() {
+	v := secret.File("My key")
+	err := v.Set("ze key", "ze value")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	value, err := v.Get("ze key")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	fmt.Println(value)
+}
